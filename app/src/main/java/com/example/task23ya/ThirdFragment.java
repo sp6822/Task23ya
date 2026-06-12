5984299226c7e6a2211b86ccc99cab962d4a2178
sp6822 <sp6822@bs.amalne.k12.il>
2026-06-13 02:01:55 +0300

    Firebase Realtime Database

--- app/src/main/java/com/example/task23ya/ThirdFragment.java
+++ app/src/main/java/com/example/task23ya/ThirdFragment.java
@@ -25,6 +25,7 @@ public class ThirdFragment extends Fragment {
     private Button btnSearchFilter;
     private RecyclerView rvResults;
 
+
     private List<Expense> allExpenses = new ArrayList<>();
     private List<Expense> filteredList = new ArrayList<>();
     private ExpenseAdapter adapter;
